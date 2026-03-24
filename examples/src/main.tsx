fc606d34c1cd72062425629e4948a5a7a6564d23
kaundur <hello@kaundur.com>
2026-03-24 07:44:57 +0000

    Updated the main.tsx in the examples to contain a root modalyze component. This fixes problems when changing between examples

--- examples/src/main.tsx
+++ examples/src/main.tsx
@@ -3,11 +3,14 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
+import { Modalyze } from 'modalyze';
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
-        <BrowserRouter>
-            <App />
-        </BrowserRouter>
+        <Modalyze>
+            <BrowserRouter>
+                <App />
+            </BrowserRouter>
+        </Modalyze>
     </StrictMode>
 );
