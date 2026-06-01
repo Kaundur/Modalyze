1de952e16d4312fe89b06d732b3e5709c0e48e40
kaundur <hello@kaundur.com>
2026-06-01 08:48:14 +0100

    Relaxed eslint rules for test files

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -46,4 +46,10 @@ export default [
       'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
     },
   },
+  {
+    files: ['**/__tests__/**/*.{ts,tsx}', '**/*.test.{ts,tsx}'],
+    rules: {
+      '@typescript-eslint/no-unsafe-argument': 'off',
+    },
+  },
 ];
